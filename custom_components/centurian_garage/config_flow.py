dc438ba37de18a3800a750155397984a97f8139c
JalviAnkur <ankurjalvi@gmail.com>
2025-03-14 12:07:21 +1100

    Update config_flow.py

--- custom_components/centurian_garage/config_flow.py
+++ custom_components/centurian_garage/config_flow.py
@@ -1,1 +1,26 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from . import DOMAIN
+
+CONF_IP = "local_ip"
+CONF_KEY = "api_key"
+
+class CenturionGarageConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Centurion Garage."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+        if user_input is not None:
+            return self.async_create_entry(title="Centurion Garage", data=user_input)
+        
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_IP): str,
+                vol.Required(CONF_KEY): str,
+            }
+        )
+        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
 
