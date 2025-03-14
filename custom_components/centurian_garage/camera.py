2d6927f055ead12824e9a89172ed65a642706db3
JalviAnkur <ankurjalvi@gmail.com>
2025-03-14 12:19:13 +1100

    Update camera.py

--- custom_components/centurian_garage/camera.py
+++ custom_components/centurian_garage/camera.py
@@ -1,6 +1,15 @@
 import requests
 from homeassistant.components.camera import Camera
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
 from . import DOMAIN
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
+    """Set up Centurion Garage Camera entity from a config entry."""
+    data = hass.data[DOMAIN][entry.entry_id]
+    async_add_entities([CenturionGarageCamera(data["local_ip"])])
 
 class CenturionGarageCamera(Camera):
     """Camera entity for Centurion Garage Door."""
