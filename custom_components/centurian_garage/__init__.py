12d3e6bf4aa46d9c257d1c2b763f4dafbf706dd1
JalviAnkur <ankurjalvi@gmail.com>
2025-03-14 12:18:34 +1100

    Update __init__.py

--- custom_components/centurian_garage/__init__.py
+++ custom_components/centurian_garage/__init__.py
@@ -1,8 +1,11 @@
 """Centurion Garage Door Integration."""
-from homeassistant.core import HomeAssistant
+import logging
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
 DOMAIN = "centurion_garage"
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the integration using YAML (deprecated)."""
@@ -10,14 +13,20 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up the integration from a config entry."""
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "cover"))
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "camera"))
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
+
+    # Forward setup to the cover and camera platforms
+    await hass.config_entries.async_forward_entry_setup(entry, "cover")
+    await hass.config_entries.async_forward_entry_setup(entry, "camera")
+
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload the integration when removed."""
+    await hass.config_entries.async_forward_entry_unload(entry, "cover")
+    await hass.config_entries.async_forward_entry_unload(entry, "camera")
+
     if entry.entry_id in hass.data[DOMAIN]:
         del hass.data[DOMAIN][entry.entry_id]
+
     return True
